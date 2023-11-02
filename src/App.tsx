8986e91f468b14c97d89351074d5de5bf15af97e
BRIAN BETT <+>
2023-11-02 22:56:40 +0300

    🚧: feat: added about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <HomePage />     
+      <About />
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <div className="about-container">
+      <h1>How we see it differently</h1>
+      <p>The global South is rapidly catching up with the "developed" world. It's time for you to get to know the rapid development , innovation and drive going on in this part of the world(East-Africa, South Asia and beyond).</p>
+      <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,w_1280/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png" alt="About Us" />
+    </div>
+  );
+};
+
+export default About;
