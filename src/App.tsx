f8ae9e7b1a53a2ca93fae5c305248fa1ef4d8b18
BRIAN BETT <+>
2023-11-02 23:37:26 +0300

    ✨: feat: added more information

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import About from './components/About';
+import Information from './components/Information';
 
 function App() {
   return (
     <div className="App">
       <HomePage />     
       <About />
+      <Information />
     </div>
   );
 }
---
+++ src/components/Information.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Information: React.FC = () => {
+  return (
+    <>  
+     <div className="info-image">
+     <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690303/elewa-group-website/Images/IMG_6523_vabpyo.png" />
+     </div>
+    <div className="info-container">
+      <h1>Elewa NV, a multi-stakeholder cooperation</h1>
+      <p> At the heart of Elewa lies the structure Elewa NV. Based in Brussels, Belgium, Elewa NV is a private holding company which controls the assets of all Elewa activities.</p>
+      <p>Elewa NV is owned and controlled by Elewa's founder, a small community of investors which share in the vision of Elewa and Elewa employees based throughout the activities of the group.</p>
+      </div>
+   </>
+  );
+};
+
+export default Information;
