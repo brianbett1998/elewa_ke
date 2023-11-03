555d9eb41529ae159deeeae73abb8c33cca80f15
BRIAN BETT <+>
2023-11-03 12:12:38 +0300

    💄: feat: added the footer and partners component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 import HomePage from './components/HomePage';
 import HomeBlack from './components/HomeBlack';
 import Cooperative from './components/Cooperative';
+import Business from './components/Business';
+import Knowledge from './components/Knowledge';
+import Partners from './components/Partners';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <HomePage/>
       <HomeBlack/>
       <Cooperative/>
+      <Business/>
+      <Knowledge/>
+      <Partners/>
+      <Footer/>
 
     </div>
   );
---
+++ src/components/Business.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Business= () => {
+  return (
+    <div className='business'>
+        <div className='business-image'>
+            <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,q_90,w_400/v1675690301/elewa-group-website/Images/IMG_0764_twyo3k.png" alt="Business-image" />
+        </div>
+     <div className="business-h1">
+      <h1>Beyond business </h1>
+      </div>
+      <div className='business-p'>
+      <p>We channel a good amount of our resources into giving back.</p>
+      <p>Through projects such as our coding school in Kakuma refugee camp, training teachers in pastoral Samburu and our open-source coding initiatives providing entry-level opportunities to hundreds of junior engineers</p>
+      <p>These projects don't necessarily have an immediate financial return, but their impact can be tangibly measured in the long-term.</p>
+     
+    </div>
+</div>
+  );
+};
+
+export default Business;
