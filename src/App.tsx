8060174d3208de226924422544026755e10acbca
BRIAN BETT <+>
2023-11-03 11:12:10 +0300

    ✨: sparkles: created the Cooperative Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
 import HomeBlack from './components/HomeBlack';
+import Cooperative from './components/Cooperative';
 
 function App() {
   return (
     <div>
       <HomePage/>
       <HomeBlack/>
-        
+      <Cooperative/>
+
     </div>
   );
 }
